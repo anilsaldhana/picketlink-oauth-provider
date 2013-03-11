93488f81daec064c202fcedc4b6536e924f45772
Anil Saldhana <Anil.Saldhana@jboss.com>
2013-03-11 15:21:35 -0500

    idm change

--- server/src/main/java/org/picketlink/oauth/provider/setup/SecurityInitializationEventHandler.java
+++ server/src/main/java/org/picketlink/oauth/provider/setup/SecurityInitializationEventHandler.java
@@ -29,7 +29,7 @@ import javax.inject.Inject;
 import org.jboss.logging.Logger;
 import org.picketbox.core.event.InitializedEvent;
 import org.picketlink.idm.IdentityManager;
-import org.picketlink.idm.credential.internal.Password;
+import org.picketlink.idm.credential.Password;
 import org.picketlink.idm.model.SimpleUser;
 import org.picketlink.idm.model.User;
 
